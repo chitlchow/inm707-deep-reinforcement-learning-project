959824c26d28455da9c6b49269e844e7540862b6
Chit Lun Chow <chitlchow@gmail.com>
2023-03-27 13:40:28 +0100

    Modularizing the code into classes

--- main.py
+++ main.py
@@ -1,84 +1,18 @@
 import random
 import pygame
+from src.snake import Snake
+from src.food import Food
+
 
 # Environment Parameters
-screen_width = 1280
-screen_height = 720
+screen_width = 500
+screen_height = 500
 grid_size = 20
 black = (0, 0, 0)
 white = (255, 255, 255)
 # Game clock
 clock = pygame.time.Clock()
 
-class Snake:
-    def __init__(self):
-        # Starting the snakes at the middle of the screen
-        self.x = screen_width//2
-        self.y = screen_height//2
-        # Initialize a random direction for the snake to start
-        self.direction = random.choice(["up","down" ,"left", "right"])
-        # Set the color of the snake to Green
-        self.color = (0, 200, 0) # Color of the snake
-
-    def move(self):
-        # Get the current x and y position
-        x, y = self.x, self.y
-
-        # Chnage the position based on the direction it is moving
-        if self.direction == "up":
-            y -= grid_size
-        elif self.direction == 'down':
-            y += grid_size
-        elif self.direction == 'left':
-            x -= grid_size
-        else:
-            x += grid_size
-
-        # If the snake is going out of the boundary in the x-direction
-        if x > 1280:
-            x = 0
-        elif x < 0:
-            x = 1280
-
-        # If the snake is going out of the boundary in the y-direction
-        if y > 720:
-            y = 0
-        elif y < 0:
-            y = 720
-
-        # Return the x and y coordinates
-        return x, y
-
-    def change_direction(self, direction):
-
-        if direction == 'up' and self.direction != 'down':
-            self.direction = 'up'
-        elif direction == 'down' and self.direction != 'up':
-            self.direction = 'down'
-        elif direction == 'left' and self.direction != 'right':
-            self.direction = 'left'
-        elif direction == 'right' and self.direction != 'left':
-            self.direction = 'right'
-
-    def show_snake(self, screen):
-        pygame.draw.rect(screen, self.color, [self.x, self.y, grid_size, grid_size])
-
-
-class Food:
-    def __init__(self):
-        # Initialize the food in a random position
-        self.x = random.randrange(0, 1280, 20)
-        self.y = random.randrange(0, 720, 20)
-        self.color = (0, 0, 200)
-
-    def move_to_random_position(self, screen):
-        pygame.draw.rect(screen, white, [self.x, self.y, grid_size, grid_size])
-        self.x = random.randrange(0, 1280, 20)
-        self.y = random.randrange(0, 720, 20)
-        self.show_food(screen)
-
-    def show_food(self, screen):
-        pygame.draw.rect(screen, self.color, [self.x, self.y, grid_size, grid_size])
 
 # Define a main function
 def main():
@@ -86,28 +20,39 @@ def main():
     pygame.init()
     pygame.display.set_caption("Snake Game")
     snake = Snake()
-    food = Food()
+    food = Food(x=random.randrange(0, screen_width, 20), y=random.randrange(0, screen_height, 20))
     screen = pygame.display.set_mode((screen_width, screen_height))
     screen.fill(white)
     score = 0
     # Define a variable to control the main loop
-    running = True
+    game_over = False
+
     # Main loop
-    while running:
+    while not game_over:
+        snake.show_snake(screen)
+        food.show_food(screen)
+
         # Event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
-                running = False
-        snake.show_snake(screen)
-        food.show_food(screen)
+                game_over = False
+
+        # Check if the snake eat the food - means they have the same coordinate
         if (snake.x, snake.y) == (food.x, food.y):
+            # Add score
             score += 1
+            # Move the food object to a different position
             food.move_to_random_position(screen)
+            # Print out the score that the snake got
             print("Score: {}".format(score))
+
+        # Get key pressed
         keys_pressed = pygame.key.get_pressed()
 
+        # Move the snake according the directional key pressed
+        # The pygame.KEY_Name returns the ASCII number of the key
         if keys_pressed[pygame.K_UP]:
             direction = 'up'
         elif keys_pressed[pygame.K_DOWN]:
@@ -118,6 +63,11 @@ def main():
             direction = 'left'
         else:
             direction = snake.direction
+
+        # If the snake hit the boarder, terminate game and break the loop
+        if snake.hit_boarder():
+            pygame.quit()
+            break
         snake.change_direction(direction)
         pygame.draw.rect(screen, white, [snake.x, snake.y, grid_size, grid_size])
         snake.x, snake.y = snake.move()
